f43dcc16dcee6b3c6e7c950b28aa237be20081be
Caleb Allen <XDDudeGuy416@protonmail.ch>
2022-09-24 16:19:33 -0400

    cargo fmt go vroom

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod en_str;
-use en_str::en_str::{Gender, BathroomVariant, Person, Type};
+use en_str::en_str::{BathroomVariant, Gender, Person, Type};
 
 fn main() {
     // counters
@@ -8,8 +8,8 @@ fn main() {
     loop {
         let mut people: Vec<Person> = vec![];
         // amount of people going to the bathroom
-        let chance = rand::random::<u32>()%400+51;
-        
+        let chance = rand::random::<u32>() % 400 + 51;
+
         // debugging
         /*
             println!("{:?}", chance.clone());
@@ -66,9 +66,9 @@ fn main() {
         }
         // simulating those people and checking if it was a success
         if simulate(people) {
-            success +=1;
+            success += 1;
         } else {
-            failures +=1;
+            failures += 1;
         }
         // printing to the console the amount of failures and successes
         println!("\nFailures: {:?}\nSuccesses: {:?}", failures, success);
@@ -78,9 +78,9 @@ fn main() {
 // (#simulate-function)
 fn simulate(people: Vec<Person>) -> bool {
     // the amount of stalls, urinals, and female stalls in the building along with the total time to do your business
-	let mut male_stalls = 9;
-	let mut urinals = 9;
-	let mut female_stalls = 10;
+    let mut male_stalls = 9;
+    let mut urinals = 9;
+    let mut female_stalls = 10;
     let mut total_time = 300;
 
     // while there is still time left
@@ -99,16 +99,16 @@ fn simulate(people: Vec<Person>) -> bool {
             }
             // (just for safety) if there is no time left leave this loop and stop immediately
             if total_time == 0 {
-                break
+                break;
             }
             // if the person is finished they don't need the bathroom anymore
             if person.finished {
-                continue
+                continue;
             }
             // count down the time remaining for people who are at a stall
             if person.at_stall {
                 person.time_remaining -= 1;
-                continue
+                continue;
             }
             // if the person is a girl and there are female stalls put them in a stall
             if person.gender == Gender::Female {
@@ -116,27 +116,33 @@ fn simulate(people: Vec<Person>) -> bool {
                     female_stalls -= 1;
                     person.at_stall = true;
                 }
-            } /* if the person is not a girl*/ else {
+            }
+            /* if the person is not a girl*/
+            else {
                 // if he has to feciate and there are stalls put him in one
                 if person.variant == BathroomVariant::Feciate {
                     if male_stalls > 0 {
                         male_stalls -= 1;
                         person.at_stall = true;
                     }
-                } /* if he has to urinate and there are urinals put him in one*/ else if urinals > 0 && person.stall_type == Type::Urinal {
+                }
+                /* if he has to urinate and there are urinals put him in one*/
+                else if urinals > 0 && person.stall_type == Type::Urinal {
                     urinals -= 1;
                     person.at_stall = true;
-                } /* if there are stalls,
-                    no urinals,
-                    and the person has to urinate,
-                    put him in a stall*/ else if urinals == 0 && person.stall_type == Type::Urinal && male_stalls > 0 {
+                }
+                /* if there are stalls,
+                no urinals,
+                and the person has to urinate,
+                put him in a stall*/
+                else if urinals == 0 && person.stall_type == Type::Urinal && male_stalls > 0 {
                     male_stalls -= 1;
                     person.at_stall = true;
                 }
             }
             // if there are no stalls or urinals the person just has to wait
-            continue
-	    }
+            continue;
+        }
         // tick down the total time
         if total_time != 0 {
             total_time -= 1;
@@ -149,7 +155,7 @@ fn simulate(people: Vec<Person>) -> bool {
 fn check_satisfaction(people: Vec<Person>) -> bool {
     for person in people {
         if person.finished != true {
-            return person.finished
+            return person.finished;
         }
     }
     true
